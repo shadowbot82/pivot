f3a303052c107833ddc0ce33001c6da6172f09cf
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-10-11 13:12:32 +0000

    Update chaust.php

--- chaust.php
+++ chaust.php
@@ -2,6 +2,6 @@
 //masukan config token yg terdapat pada link di packet captur
 // jangan hapus tanda kutip(" ") 
 
-$access_token = "c5a65f71-ecbe-46a0-b011-a8125ae04878";
+$session = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 // kamu bisa membuat banyak config untuk beberapa akun.
